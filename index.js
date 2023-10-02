ab1439b5479877a4468706012bcf20fabde5a11b
sagor <sagorislam9936@gmail.com>
2023-10-02 21:52:24 +0600

    Add search bar

--- index.js
+++ index.js
@@ -1,5 +1,71 @@
+const postId = document.getElementById("postId");
+const SeePost = document.getElementById("SeePost");
+
+const ViewPost = document.getElementById("ViewPost");
+
 const users = document.getElementById("users");
 const signleCountry = document.getElementById("singleCountry");
+
+
+
+const showPost = () => {
+  const name = postId.value;
+
+  fetch(`https://restcountries.com/v3.1/name/${name}`)
+    .then((response) => response.json())
+
+    .then((json) => showAllCountriess(json));
+};
+
+const showAllCountriess = (countries) => {
+  console.log(countries);
+  countries.map((country) => {
+    const {
+      name: { common: countryName },
+      capital,
+      population,
+      region,
+      flags: { png },
+    } = country;
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("m-auto");
+    newDiv.innerHTML = `
+    <div class="py-3 h-100 ">
+    <div class="card  h-100 bg-secondary-subtle text-center shadow rounded-5 p-1 ">
+  <div class="card-body  text-primary-emphasis ">
+  <img src=${png} style="width:75px">
+  <h3 class="fs-2 " style=color:red;>Name: ${countryName}</h3>
+  <p>Capital: ${capital}</p>
+  <p>Region: ${region}</p>
+  <p>Population: ${population}</p>
+  <button class=" bg-success primary rounded-5" onclick="showSingleCountry ('${countryName}')">
+  Details</button>
+  </div>
+  </div>
+  </div>`;
+  ViewPost.innerHTML="";
+  ViewPost.appendChild(newDiv);
+  });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 fetch("https://restcountries.com/v3.1/all")
   .then((response) => response.json())
